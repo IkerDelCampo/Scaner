760da3a73681c166087d00c854d340a2b152148c
IkerDelCampo <rekidelcampo@gmail.com>
2021-09-23 09:57:29 +0200

    Initial commit

---
+++ src/scaner/Scaner.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scaner;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rey
+ */
+public class Scaner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Bunas como te llamas?");
+        String nombre=teclado.nextLine();
+        System.out.println("nombre = " + nombre);
+    }
+    
+}
